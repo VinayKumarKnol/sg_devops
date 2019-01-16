65ab668c78aeee9cf17e4425cfafa805b9362af9
Sudeep James Tirkey <sudeep.james@knoldus.in>
2019-01-16 15:51:58 +0530

    No comments

--- destroy.sh
+++ destroy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 gcloud auth activate-service-account --key-file google-key.json
-gcloud compute instances delete instance-template-1
+gcloud compute instances delete INSTANCE_NAMES instance-template-1
